f74e8dd5376be74b2dfd53b82c0b899e832000af
Arjun Lall <lallarjun@gmail.com>
2017-05-23 16:35:23 +0000

    refactored for foreach loop going through all rows if multiple results

--- test_script.js
+++ test_script.js
@@ -33,12 +33,7 @@ let findName = (name, cb) => {
 let findNameOutput = (result) => {
   console.log('Searching...');
   console.log(`Found ${result.rows.length} person(s) by the name '${name}'`);
-  console.log(`-${result.rows[0].id}: ${result.rows[0].first_name} ${result.rows[0].last_name}, born '${result.rows[0].birthdate}'`); //output: 1
+  result.rows.forEach((row) => {
+      console.log(`-${row.id}: ${row.first_name} ${row.last_name}, born '${row.birthdate}'`);
+  });
 };
-
-  // client.query("SELECT $1::int AS number", ["1"], (err, result) => {
-  //   if (err) {
-  //     return console.error("error running query", err);
-  //   }
-  //   console.log(result.rows[0].number); //output: 1
-  // });
